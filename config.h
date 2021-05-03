cb0cfe85dc52b763c598890db490fea525fd8fb0
3090 <my_server@edu.cn>
2021-05-03 17:27:03 +0800

    change windows set

--- config.h
+++ config.h
@@ -17,8 +17,8 @@ static const int smartgaps          = 1;        /* 1 means no outer gap when the
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 0;        /* 0 means bottom bar */
 static const Bool viewontag         = True;     /* Switch view on tag switch */
-static const char *fonts[]          = { "SauceCodePro Nerd Font Mono:size=13" };
-static const char dmenufont[]       = "SauceCodePro Nerd Font Mono:size=15";
+static const char *fonts[]          = { "SauceCodePro Nerd Font Mono:size=12" };
+static const char dmenufont[]       = "SauceCodePro Nerd Font Mono:size=25";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
@@ -80,7 +80,7 @@ static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() 
 //static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *dmenucmd[] = {"dmenu_run",NULL};
 static const char *termcmd[]  = { "st", NULL };
-static const char *browsercmd[]  = { "chromium-web-browser", NULL };
+static const char *browsercmd[]  = { "google-chrome", NULL };
 
 static const char *upvol[]   = { "/home/zcdu/scripts/vol-up.sh",  NULL };
 static const char *downvol[] = { "/home/zcdu/scripts/vol-down.sh",  NULL };
@@ -90,7 +90,7 @@ static const char *wpcmd[]  = { "/home/zcdu/scripts/wp-change.sh", NULL };
 static const char *sktogglecmd[]  = { "/home/zcdu/scripts/sck-tog.sh", NULL };
 static const char scratchpadname[] = "scratchpad";
 static const char *scratchpadcmd[] = { "st", "-t", scratchpadname, "-g", "80x24", NULL };
-static const char *alacrittycmd[] = {"alacritty", NULL};
+static const char *alacrittycmd[] = {"gnome-terminal", NULL};
 
 static const char *suspendcmd[]  = { "/home/zcdu/scripts/suspend.sh", NULL };
 
@@ -122,17 +122,17 @@ static Key keys[] = {
 	{ MODKEY,              XK_Tab,                  view,           {0} },
 	{ MODKEY|ShiftMask,    XK_q,                    killclient,     {0} },
 	{ MODKEY,              XK_t,                    setlayout,      {.v = &layouts[0]} },
+	{ MODKEY,              XK_n,                    setlayout,      {.v = &layouts[1]} },
 	{ MODKEY,              XK_m,                    setlayout,      {.v = &layouts[2]} },
 	{ MODKEY|ShiftMask,    XK_f,                    fullscreen,     {0} },
-	{ MODKEY,              XK_space,                setlayout,      {0} },
 	{ MODKEY|ShiftMask,    XK_space,                togglefloating, {0} },
 	{ MODKEY,              XK_w,                    togglescratch,  {.v = scratchpadcmd } },
 	{ MODKEY,              XK_0,                    view,           {.ui = ~0 } },
 	{ MODKEY|ShiftMask,    XK_0,                    tag,            {.ui = ~0 } },
-	{ MODKEY,              XK_comma,                focusmon,       {.i = -1 } },
-	{ MODKEY,              XK_period,               focusmon,       {.i = +1 } },
-	{ MODKEY|ShiftMask,    XK_comma,                tagmon,         {.i = -1 } },
-	{ MODKEY|ShiftMask,    XK_period,               tagmon,         {.i = +1 } },
+	{ MODKEY,              XK_o,                focusmon,       {.i = -1 } },
+	{ MODKEY,              XK_p,               focusmon,       {.i = +1 } },
+	{ MODKEY|ShiftMask,    XK_o,                tagmon,         {.i = -1 } },
+	{ MODKEY|ShiftMask,    XK_p,               tagmon,         {.i = +1 } },
 	TAGKEYS(               XK_1,                      0)
 	TAGKEYS(               XK_2,                      1)
 	TAGKEYS(               XK_3,                      2)
