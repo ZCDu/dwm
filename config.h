f55688b52e96580174a476cf5d1d6af75f52f52c
3090 <my_server@edu.cn>
2021-11-28 18:42:52 +0800

    feat(rofi): change dmenu to rofi
    config.h dwm.c

--- config.h
+++ config.h
@@ -76,9 +76,9 @@ static const Layout layouts[] = {
 #define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
 
 /* commands */
-static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
+//static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 //static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *dmenucmd[] = {"dmenu_run",NULL};
+static const char *dmenucmd[] = {"rofi", "-show", "drun", NULL }; 
 static const char *termcmd[]  = { "st", NULL };
 static const char *browsercmd[]  = { "google-chrome", NULL };
 
--- dwm.c
+++ dwm.c
@@ -2185,8 +2185,6 @@ sigchld(int unused)
 void
 spawn(const Arg *arg)
 {
-	if (arg->v == dmenucmd)
-		dmenumon[0] = '0' + selmon->num;
 	selmon->tagset[selmon->seltags] &= ~scratchtag;
 	if (fork() == 0) {
 		if (dpy)
